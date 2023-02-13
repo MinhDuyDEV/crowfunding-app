482d5616e3bb0fbfdcece8b506350e557aae546f
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2023-02-13 10:07:25 +0700

    setup Axios Interceptor

--- src/components/api/axios.js
+++ src/components/api/axios.js
@@ -3,3 +3,10 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://localhost:5000",
 });
+
+export const axiosPrivate = axios.create({
+  baseURL: "https://localhost:4001",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
